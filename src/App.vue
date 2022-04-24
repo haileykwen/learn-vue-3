da46fc08b550da2518230244252ee19e38110db0
haileykwen <anonyhailey@gmail.com>
2022-04-24 11:34:02 +0700

    5. Binding Text

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    Hello {{ name }}
+    <div v-text="age"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      name: "harleykwen",
+      age: 22
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
